e8b9e278371537fe8fc2d36cac0a0e347503813e
CdeJong <christiaan240@gmail.com>
2022-06-26 08:16:27 +0200

    v1

---
+++ src/main/java/com/shebbasoft/nomorestackedarmor/NoMoreStackedArmor.java
@@ -0,0 +1,89 @@
+package com.shebbasoft.nomorestackedarmor;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class NoMoreStackedArmor extends JavaPlugin implements Listener {
+
+    private static final int MAX_ARMOR_AMOUNT = 1;
+    private static final String ARMOR_REMOVED_MESSAGE = ChatColor.RED + "" + ChatColor.BOLD + "[!] Your equipped stacked armor got removed from your inventory.";
+    private static final Set<Material> ARMOR_ITEMS = new HashSet<>();
+
+    static {
+        ARMOR_ITEMS.add(Material.DIAMOND_HELMET);
+        ARMOR_ITEMS.add(Material.DIAMOND_CHESTPLATE);
+        ARMOR_ITEMS.add(Material.DIAMOND_LEGGINGS);
+        ARMOR_ITEMS.add(Material.DIAMOND_BOOTS);
+        ARMOR_ITEMS.add(Material.GOLD_HELMET);
+        ARMOR_ITEMS.add(Material.GOLD_CHESTPLATE);
+        ARMOR_ITEMS.add(Material.GOLD_LEGGINGS);
+        ARMOR_ITEMS.add(Material.GOLD_BOOTS);
+        ARMOR_ITEMS.add(Material.IRON_HELMET);
+        ARMOR_ITEMS.add(Material.IRON_CHESTPLATE);
+        ARMOR_ITEMS.add(Material.IRON_LEGGINGS);
+        ARMOR_ITEMS.add(Material.IRON_BOOTS);
+        ARMOR_ITEMS.add(Material.CHAINMAIL_HELMET);
+        ARMOR_ITEMS.add(Material.CHAINMAIL_CHESTPLATE);
+        ARMOR_ITEMS.add(Material.CHAINMAIL_LEGGINGS);
+        ARMOR_ITEMS.add(Material.CHAINMAIL_BOOTS);
+        ARMOR_ITEMS.add(Material.LEATHER_HELMET);
+        ARMOR_ITEMS.add(Material.LEATHER_CHESTPLATE);
+        ARMOR_ITEMS.add(Material.LEATHER_LEGGINGS);
+        ARMOR_ITEMS.add(Material.LEATHER_BOOTS);
+    }
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onArmorEquip(PlayerInteractEvent event) {
+
+        Action action = event.getAction();
+
+        if (!action.equals(Action.RIGHT_CLICK_AIR) && !action.equals(Action.RIGHT_CLICK_BLOCK)) {
+            return;
+        }
+
+        ItemStack item = event.getItem();
+
+        if (item == null || item.getAmount() <= MAX_ARMOR_AMOUNT || !ARMOR_ITEMS.contains(item.getType())) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        PlayerInventory inventory = event.getPlayer().getInventory();
+        boolean removedArmor = false;
+
+        for (ItemStack armor : inventory.getArmorContents()) {
+            if (armor == null || armor.getAmount() <= MAX_ARMOR_AMOUNT || !ARMOR_ITEMS.contains(armor.getType())) {
+                continue;
+            }
+
+            armor.setAmount(MAX_ARMOR_AMOUNT);
+            removedArmor = true;
+        }
+
+        if (removedArmor) {
+            event.getPlayer().sendMessage(ARMOR_REMOVED_MESSAGE);
+        }
+    }
+}
